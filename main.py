0d4b063913798b6dcee3fcc39e151e7c00b2d1fa
Anton Kurakin <anton.kurakin@gmail.com>
2022-05-12 21:40:00 +0300

    2022-01-07 16:30

--- main.py
+++ main.py
@@ -7,26 +7,35 @@ server = 'webapi.educationvector.com'
 manager = '1003'
 password = 'hf7jrf83er'
 
-# Получаем значение account из внешней DB
-account = '111644'
+""" accounts_list - переменная типа "Множество (SET) {"25362", "58741", "46578" ...}" """
 
-# Устанавливаем соединение с MT5
-session = mt5.connect(server, manager, password)
 
-# Запрещаем торговлю на счету account
-mt5.mod_account_rights(server, account, session, 16743)
+def check_account_by_limit(accounts_list: set):
+    """ Функция запрещает торговлю на счету и
+    закрывает все открытые и отложенные ордера """
 
-# Закрываем отложенные ордера
-orders_count = mt5.del_order(server, account, session)
-if orders_count > 0:
-    while orders_count > 0:
+    # Устанавливаем соединение с MT5
+    session = mt5.connect(server, manager, password)
+
+    for account in accounts_list:
+        # Запрещаем торговлю на счету account
+        mt5.mod_account_rights(server, account, session, 16743)
+
+        # Закрываем отложенные ордера
         orders_count = mt5.del_order(server, account, session)
-        time.sleep(0.5)
-# Закрываем открытые позиции
-positions_count = mt5.pcount(server, account, session)
-if positions_count > 0:
-    while positions_count > 0:
-        positions_count = mt5.close_position(server, manager, password, account, session)
-        time.sleep(0.5)
+        if orders_count > 0:
+            while orders_count > 0:
+                orders_count = mt5.del_order(server, account, session)
+                time.sleep(0.5)
+        # Закрываем открытые позиции
+        positions_count = mt5.pcount(server, account, session)
+        if positions_count > 0:
+            while positions_count > 0:
+                positions_count = mt5.close_position(server, manager, password, account, session)
+                time.sleep(0.5)
 
 
+accounts_list = {"789879", "111777", "111546", "111445"}
+
+if __name__ == "__main__":
+    check_account_by_limit(accounts_list)
